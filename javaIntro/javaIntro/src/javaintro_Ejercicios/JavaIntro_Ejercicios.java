8f60c8b2685718d1a850afa5d342bbea68dcfdaf
0xnaen <0xnaendev@gmail.com>
2023-05-25 11:57:01 -0500

    Java hasta el Ejer 5

--- javaIntro/javaIntro/src/javaintro_Ejercicios/JavaIntro_Ejercicios.java
+++ javaIntro/javaIntro/src/javaintro_Ejercicios/JavaIntro_Ejercicios.java
@@ -3,6 +3,8 @@
  */
 package javaintro_Ejercicios;
 
+import java.util.Scanner;
+
 /**
  *
  * @author nager
@@ -52,7 +54,53 @@ public class JavaIntro_Ejercicios {
    
    // Ejercicio 4 ! Define una variable que aloje tu nombre y otra que guarde tu edad. Imprime ambas variables por pantalla
     
-    System.out.println("Su nombre es: "+ name + " y su edad es: "+ old);
+    System.out.println("Su nombre del developer es: "+ name + " y su edad es: "+ old);
+    
+    
+    // Ejercicio 5  ! Define una variable de tipo ¿boolean?, double y char. Guarda información en ellas a través del Scanner.
+    
+    String name2, lastName2;
+    int old2;
+    
+    Scanner input = new Scanner(System.in);
+        System.out.println("Ingrese su Nombre:");
+        name2 = input.nextLine();
+        System.out.println("Ingrese Su apellido");
+        lastName2 = input.nextLine();
+        System.out.println("Ingrese su edad");
+        old2 = input.nextInt();
+        
+        System.out.println("");
+        
+        mayorQue = old > old2;
+        
+        System.out.println("Es "+ name +" mayor que "+ name2 + " : " + mayorQue);
+        
+        // Ejerjercicio 6 ! Implementar un programa que le pida dos números enteros a usuario y determine si ambos o alguno de ellos es mayor a 25.
+       
+        
+        int venticinco = 25;
+        
+        System.out.println("Ingrese un número");
+        int number1 = input.nextInt();
+        System.out.println("Ingrese otro número");
+        int number2 = input.nextInt();
+        
+        if (number1 == venticinco && number2 == venticinco){
+            System.out.println("Los 2  número ingresados es igual a : "+ venticinco);
+        }else if (number1 < venticinco && number2 < venticinco){
+                    System.out.println("Ambos número son menores a: "+venticinco);
+                    
+        }else if (number1 < venticinco && number2 > venticinco) {
+            
+            System.out.println("El 1er número es menor a: " + venticinco);
+            
+        }else {System.out.println("El 2do número es menor a: "+ venticinco);}
+                    
+                    }
+        }
+        
+        
     
     
     }

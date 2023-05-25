2367b07d227f2b9e6a153f946c2631c512b58702
0xnaen <0xnaendev@gmail.com>
2023-05-25 10:18:08 -0500

    Atc

---
+++ javaIntro/javaIntro_Ejercicios/src/javaintro_Ejercicios/JavaIntro_Ejercicios.java
@@ -0,0 +1,58 @@
+/*
+ * Crear un proyecto de Java y definir al menos 6 variables en tu IDE de distintos tipos de datos.
+ */
+package javaintro_Ejercicios;
+
+/**
+ *
+ * @author nager
+ */
+public class JavaIntro_Ejercicios {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        // Ejer 1 !Declaraciones de Variables
+        String name; 
+        int old;
+        char sex;
+        double sueldo;
+        boolean aux;
+        long id;
+        
+        // Ejer 2 !Agsinacion de datos a las Variables
+        
+        name = "NaenDev";
+        old = 40;
+        sex = 'M';
+        sueldo = 220.5;
+        aux = true;
+        id = 123456789;
+        
+        
+    }
+    
+    
+    // Ejercicio 3 !Define variables donde puedas alojar los resultados y prueba usar dos operadores de cada tipo.
+        
+    int num1 = 4;
+    int num2 = 5;
+    
+  
+    int suma = num1 + num2;   // Suma
+        
+    int resta = num2 - num1; //Resta
+    
+    double division = num1 / num2; // Division
+        
+    boolean mayorQue = num2 > num1;  // Boolean
+    
+   
+   // Ejercicio 4 ! Define una variable que aloje tu nombre y otra que guarde tu edad. Imprime ambas variables por pantalla
+    
+    System.out.println(“Hola Mundo”);
+
+}
+
